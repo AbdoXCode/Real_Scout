4c7b65682cae75addc0c5921ca8c5d1736005e56
Abdelrhman <adodoadodo2025@gmail.com>
2026-02-11 00:50:35 +0200

    add new screens and layout updates; implement font loading and splash screen handling

---
+++ app/(root)/properties/[id].jsx
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native'
+import React from 'react'
+import {useLocalSearchParams} from "expo-router"
+
+export default function Property() {
+    const {id} = useLocalSearchParams()
+    return (
+        <View>
+            <Text>Property</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({})
---
+++ app/_layout.jsx
@@ -0,0 +1,27 @@
+import {SplashScreen, Stack} from "expo-router";
+import {useFonts} from "expo-font";
+import {useEffect} from "react";
+
+
+export default function RootLayout() {
+    const [fonts] = useFonts({
+        "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+        "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+        "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+        "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+        "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+        "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+    })
+
+    useEffect(() => {
+        if (fonts) {
+            SplashScreen.hideAsync()
+        }
+    }, [fonts]);
+
+    if (!fonts) {
+        return null
+    }
+    
+    return <Stack/>;
+}
